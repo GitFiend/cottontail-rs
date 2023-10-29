f55398cf16a9e39a23a927c0a3497bf69381765a
Toby <sugto555@gmail.com>
2023-10-29 13:47:21 +1300

    Explore better way of setting styles.

--- src/style.rs
+++ src/style.rs
@@ -1,3 +1,4 @@
+use crate::style::Position::Absolute;
 use std::fmt;
 
 pub enum Style {
@@ -24,4 +25,42 @@ impl fmt::Display for Position {
       Position::Fixed => write!(f, "fixed"),
     }
   }
+}
+
+pub struct Styles {
+  styles: Vec<StyleAttribute>,
+}
+impl Styles {
+  pub fn new() -> Styles {
+    Styles { styles: Vec::new() }
+  }
+
+  pub fn background(mut self, background: &str) -> Self {
+    self
+      .styles
+      .push(StyleAttribute::BackgroundColor(background.to_string()));
+    self
+  }
+  pub fn position(mut self, position: Position) -> Self {
+    self.styles.push(StyleAttribute::Position(position));
+    self
+  }
+}
+
+enum StyleAttribute {
+  BackgroundColor(String),
+  Position(Position),
+  Left(f64),
+  Top(f64),
+  Bottom(f64),
+  Width(f64),
+  Height(f64),
+}
+
+pub fn styles() -> Styles {
+  Styles { styles: Vec::new() }
+}
+
+fn test() {
+  let style = styles().position(Absolute).background("blue");
 }
